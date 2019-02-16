fded828eb56c4cfc200c37b779d6ecae8f283171
Herbert Diaz <hrbrtdiaz@gmail.com>
2019-02-16 17:56:02 -0600

    Added comments and formatted main function and Ant files

--- Ant.hpp
+++ Ant.hpp
@@ -3,10 +3,16 @@
 ** Author:	Group 4
 ** Date:	2019/02/04
 ** Description: THIS IS THE HEADER FILE OF THE ANT CLASS
+**              This program runs a Predator-Prey Game with "Doodlebug"
+**              predators and "Ant" prey. The program starts with a menu and
+**              has the user set the number steps for the simulation to take.
+**              Once the simulation is complete, the user is queried for the
+**              whether they want to continue with more steps or quit.
 **		ABSTRACTION OF:	The Ants, or Prey, of the Game
-**		PURPOSE:
-**		HOW TO USE:
-**		SUMMARY:
+**		PURPOSE: This program encapsulates all the functions related to an Ant object.
+**		HOW TO USE: Use the constructor to create an Ant object.
+**		SUMMARY: The Ant class is a derived class of the Critter class. Use the move function to
+**      move an Ant on the board.
 **************************************************************************************************/
 
 
@@ -26,7 +32,6 @@ class Ant : public Critter
 		Ant(int x_pos, int y_pos);
 		~Ant(){};
 		void move(Critter *** &, int, int);
-	 	// void breed(Critter*** &, int, int);	// TODO: DETERMINE NECESSARY PARAMETERS ~HD
 };
 
 #endif
